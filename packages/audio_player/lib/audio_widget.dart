83c34dd5cf6ff67e457877238b5c9d488aa88387
iqthuc <thucnq@qsoft.vn>
2023-08-09 14:43:12 +0700

    init audio_palyer

---
+++ packages/audio_player/lib/audio_widget.dart
@@ -0,0 +1,13 @@
+// Copyright 2023 QSoft, Ltd. All rights reserved.
+
+
+import 'package:flutter/material.dart';
+
+class AudioWidget extends StatelessWidget {
+  const AudioWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
